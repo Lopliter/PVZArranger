d3e8cddd9978aa8a2d9d069f2f10ae59ae2423bc
Lopliter <lopliter@gmail.com>
2023-11-04 23:43:21 +0800

    呃

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function closeWindow() {
     window.close(); // 关闭当前网页
 }
+function printGarden(scenes){
+    switch (scenes){
+        case 0:
+            var gardenTable = document.getElementById("garden");
+            var gardenImg = document.getElementById("background");
+            gardenImg.src = "images/day.png";
+    }
+}
