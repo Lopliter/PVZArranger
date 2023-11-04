0bb1bf7762e749b139161ec2acc70a74eaea6d5e
Lopliter <lopliter@gmail.com>
2023-11-04 17:08:15 +0800

    主界面不要了

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 app.on('ready', () => {
   let mainWindow = new BrowserWindow({
     frame: false,
@@ -20,4 +20,24 @@ app.on('ready', () => {
   mainWindow = null;
   });
 });
-
+ipcMain.on('openNewWindow', () => {
+    let mainPage = new BrowserWindow({
+      frame: false,
+      width: 1366,
+      height: 768,
+      maxWidth: 1366,
+      minWidth: 1366,
+      maxHeight: 808,
+      minHeight: 808,
+      vibrancy: 'dark',
+      visualEffectState: "active",
+      webPreferences: {experimentalFeatures: true}
+  });
+  mainPage.loadFile('MainPage.html')
+  mainPage.setMenu(null)
+  mainPage.setMaximizable(false)
+  mainPage.removeMenu()
+  mainPage.on('closed', () => {
+    mainPage = null;
+  });
+});
